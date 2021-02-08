2d59355656ceddc21ec0518796f9d623703a339d
Kyle Pericak <kyle@pericak.com>
2021-02-08 10:21:14 -0500

    Breq 1283 multithread (#86)
    * Downloads now use wget in threads
    
    * finished multi-threaded wget based downloads
    
    * Added tabs for totals to format them better

--- voithos/lib/vmware/exporter.py
+++ voithos/lib/vmware/exporter.py
@@ -3,9 +3,16 @@ import os
 import requests
 from datetime import datetime
 from time import sleep, time
+from threading import Thread
+from pathlib import Path
 
 from hurry.filesize import size
 from pyVmomi import vim
+
+from voithos.lib.system import run
+
+
+SLEEP_INTERVAL = 30  # seconds
 
 
 class VMWareExportLeaseNotReady(Exception):
@@ -89,57 +96,121 @@ class VMWareExporter:
             f"ERROR - NFC lease state {self.lease.state} after {max_retries} retries"
         )
 
-    def print_progress(self, new_bytes_written):
-        """ Increments the byte counter and handles printing progress """
-        if self.interval_seconds <= 0:
-            # progress tracking is disabled when interval is 0
-            return
-        self.transfered_bytes += new_bytes_written
-        now = time()
-        if now <= (self.last_print + self.interval_seconds):
-            # Nothing to print, we've already printed during this interval
-            return
-        # Set interval because chunks can take longer than interval.seconds
-        interval = int(now - self.last_print)
-        self.last_print = now
-        # avoid dividing by 0 when calculating rates - divisors are in seconds
-        total_divisor = now - self.start_ts
-        total_divisor = total_divisor if total_divisor else 1
-        interval_divisor = interval if interval else 1
-        # calculate and print the transfer rates - total and in the last interval
-        total_rate = size(self.transfered_bytes / total_divisor)
-        interval_bytes = self.transfered_bytes - self.last_transfered_bytes
-        interval_rate = size(interval_bytes / interval_divisor)
-        self.last_transfered_bytes = self.transfered_bytes
-        self.percent_transfered = int(self.transfered_bytes / self.size_in_bytes * 100)
-        print(
-            str(datetime.now(tz=None)) + f" - Progress: {size(self.transfered_bytes)}"
-            f" ({self.percent_transfered}%)"
-            f" - Total rate: {total_rate}/s"
-            f" - Rate of last {interval}s: {interval_rate}/s"
-        )
-
     def download(self):
         """ Initiate the download process """
-        print(f"Total VM Size: {size(self.size_in_bytes)}")
+        downloads = []
+        # Start a thread running WGET for each vmdk in parralel
+        gb_total = bytes_to_gb(self.size_in_bytes)
+        print(f"Download {gb_total} GB:")
         for dev in self.lease_disks:
+            # Collect the download paths and filenames
+            url = dev.url.replace("*/", f"{self.vmware_mgr.ip_addr}/")
             file_path = os.path.join(self.base_dir, dev.targetId)
-            with open(file_path, "wb") as vol_file:
-                # The dev.url value is https:/*/ for some reason, replace it with the IP
-                url = dev.url.replace("*/", f"{self.vmware_mgr.ip_addr}/")
-                print(f"Downloading {url} to {file_path}")
-                headers = {"Accept": "application/x-vnd.vmware-streamVmdk"}
-                resp = requests.get(
-                    url, stream=True, headers=headers, cookies=self.cookies, verify=False
-                )
-                for block in resp.iter_content(chunk_size=(self.chunk_size)):
-                    if not block:
-                        continue
-                    vol_file.write(block)
-                    # TO DO: Try swapping len(block) with self.chunk_size for speed
-                    self.print_progress(len(block))
-                    # update the export progress in VMWare
-                    self.lease.HttpNfcLeaseProgress(self.percent_transfered)
-        # when the loop is done, set the progress to 100% (to handle edge cases)
+            print(f"  {file_path} <-- {url}")
+            thread = Thread(target=download_thread, kwargs={"url": url, "file_path": file_path})
+            thread.start()
+            downloads.append(
+                {
+                    "url": url,
+                    "file_path": file_path,
+                    "thread": thread,
+                    "last_size": 0,
+                    "size": 0,
+                    "finished_size_thick": 0,
+                    "finished_size_thin": 0,
+                    "finshed_speed": 0,
+                    "done": False,
+                }
+            )
+        sleep(2)  # Give wget a second to get going
+        print(f"  Starting download ... Progress updates every {SLEEP_INTERVAL} seconds")
+        # Every x seconds, check the file sizes and provide a status update. Also update NFC lease
+        num_downloading_files = len(downloads)
+        elapsed_seconds = 0
+        while num_downloading_files >= 1:
+            # Count the alive threads, when the count is 0 this is the last iteration
+            num_downloading_files = len([dld for dld in downloads if dld["thread"].is_alive()])
+            if num_downloading_files == 0:
+                break  # no need to wait 30 seconds at the end
+            # wait 30 seconds (or whatever SLEEP_INTERVAL is) per check
+            print("")
+            sleep(SLEEP_INTERVAL)
+            elapsed_seconds += SLEEP_INTERVAL
+            print(f"Downloading files: {num_downloading_files}/{len(downloads)} remaining")
+            downloaded_bytes_thick = 0  # re-calc'd each 30 seconds
+            downloaded_bytes_thin = 0
+            for download in downloads:
+                if download["done"]:
+                    # This VMDK download's download completion tasks have been done
+                    # use its "thick size" instead
+                    downloaded_bytes_thick += download["finished_size_thick"]
+                    print_download_progress(download, download["finished_size_thick"])
+                    downloaded_bytes_thin += download["finished_size_thin"]
+                    continue
+                if not download["thread"].is_alive():
+                    # This download just finished, find its "finished size" and mark it done
+                    download["done"] = True
+                    download["finished_size_thick"] = get_vmdk_thick_size(download["file_path"])
+                    downloaded_bytes_thick += download["finished_size_thick"]
+                    download["finished_size_thin"] = Path(download["file_path"]).stat().st_size
+                    downloaded_bytes_thin += download["finished_size_thin"]
+                    download["finished_speed"] = round(
+                        downloaded_bytes_thin / 1024 / 1024 / elapsed_seconds, 2
+                    )
+                    print_download_progress(download, download["finished_size_thick"])
+                    continue
+                # {download} is still downloading, find its current progress by checking file size
+                file_size = Path(download["file_path"]).stat().st_size  # (thin size)
+                downloaded_bytes_thick += file_size  # We don't know the real thick size yet
+                downloaded_bytes_thin += file_size
+                download["last_size"] = download["size"]
+                download["size"] = file_size
+                print_download_progress(download, file_size)  # use thin size for progress now
+            self.percent_transfered = int(downloaded_bytes_thick / self.size_in_bytes * 100)
+            self.lease.HttpNfcLeaseProgress(self.percent_transfered)
+            gb_down_thick = bytes_to_gb(downloaded_bytes_thick)
+            print(f"\- Total Downloaded: \t{gb_down_thick} GB / {gb_total} GB - {self.percent_transfered}%")
+            thick_avg_speed_mbs = round(downloaded_bytes_thick / 1024 / 1024 / elapsed_seconds, 2)
+            print(f"\- Avg Speed (thick): \t{thick_avg_speed_mbs} MB/s")
+            thin_avg_speed_mbs = round(downloaded_bytes_thin / 1024 / 1024 / elapsed_seconds, 2)
+            print(f"\- Avg Speed (thin): \t{thin_avg_speed_mbs} MB/s")
+        print("Finished download, closing NFC lease")
         self.lease.HttpNfcLeaseProgress(100)
         self.lease.HttpNfcLeaseComplete()
+
+
+def print_download_progress(download, progress):
+    """ Print the progress of a download """
+    # last interval speed
+    # total speed
+    # path
+    if download["done"]:
+        done = "(DONE)"
+        speed = f"\t[AVG SPEED: {download['finished_speed']} MB/s]"
+    else:
+        done = ""
+        diff = download["size"] - download["last_size"]
+        current_speed = round(diff / 1024 / 1024 / SLEEP_INTERVAL, 2)
+        speed = f"\t[CUR SPEED: {current_speed} MB/s]"
+    size_gb = bytes_to_gb(progress)
+    print(f"  {download['file_path']} - {size_gb} GB {speed} {done}")
+
+
+def download_thread(url, file_path):
+    """ Start a thread to download the file """
+    run(f"wget --quiet --no-check-certificate {url} -O {file_path}")
+
+
+def get_vmdk_thick_size(file_path):
+    """ Return the 'thick' size of a VMDK file in bytes as an integer - requires qemu-utils """
+    qemu_img_lines = run(f"qemu-img info {file_path}")
+    vsize_line = next(line for line in qemu_img_lines if "virtual size" in line)
+    size_bytes = int(vsize_line.split(" ")[-2].replace("(", ""))
+    return size_bytes
+
+
+def bytes_to_gb(qty_bytes):
+    """ Return a GB value of bytes, rounded to 2 decimals """
+    bytes_in_gb = 1024 * 1024 * 1024
+    qty_gb = qty_bytes / bytes_in_gb
+    return round(qty_gb, 2)
